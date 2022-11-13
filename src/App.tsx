ca14915108d672854cdb692ea6e54aed97de7576
Arnold LAMBOU <lambouarnold@gmail.com>
2022-11-13 22:05:21 +0100

    added Try for free url in the env

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,8 @@ import FooterComponent from './components/FooterComponent';
 
 function App() {
   return (
-    <div className="main-container flex items-center justify-center w-screen min-h-screen bg-slate-100">
-      <div className='card-container flex flex-col p-8 rounded-lg bg-white shadow-lg w-full max-w-4xl'>
+    <div className="main-container flex items-center justify-center w-screen min-h-screen">
+      <div className='card-container flex flex-col p-8 rounded-2xl bg-white shadow-lg w-full max-w-4xl'>
         <HeaderComponent className='header-container' />
         <HeroComponent className='hero-container' />
         <FooterComponent className='footer-container mt-3'/>
--- src/components/HeroComponent/index.tsx
+++ src/components/HeroComponent/index.tsx
@@ -1,5 +1,5 @@
 import clsx from "clsx";
-import { FC, HTMLAttributes } from "react";
+import { FC, HTMLAttributes, useCallback } from "react";
 import LargeButton from "../LargeButton";
 import UndrawWorkout from "./UndrawWorkout.svg";
 
@@ -7,11 +7,18 @@ export type HeroComponentProps = HTMLAttributes<HTMLDivElement> & {};
 
 const HeroComponent: FC<HeroComponentProps> = (props) => {
     const { className, ...restProps } = props;
+
+    const btnClickHandler = useCallback(() => {
+        if (process.env.REACT_APP_TRY_FOR_FREE_URL) {
+            window.open(process.env.REACT_APP_TRY_FOR_FREE_URL);
+        }
+    }, [])
+
     return <div className={clsx([className, "flex items-center gap-8 py-8"])} {...restProps}>
         <div className="flex flex-col items-start gap-6 max-w-sm">
             <h1 className="capitalize font-extrabold text-5xl">It's time to regain your fitness</h1>
             <p className="text-gray-500 text-base">Do fitness anywhere and anytime with our training videos.</p>
-            <LargeButton type="button">Try it for free</LargeButton>
+            <LargeButton type="button" onClick={btnClickHandler}>Try it for free</LargeButton>
         </div>
         <div className="flex-auto">
             <img src={UndrawWorkout} alt="Workout" width={0} height={0} className="object-contain w-full h-full" />
